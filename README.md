dd0ee4fd700cde48d8b86f411c2d7d2361d17207
pdhar <pranav.dhar2@gmail.com>
2016-03-07 22:29:24 -0500

    added README file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Rocketrip Assignment
 
+* Python version: Tested on version 2.7.3, but should work on latest version.
+
+* Instructions: python rocketrip_assignment.py "keyword"
+
+* Dependencies:
+
+	* oauth2 	 - https://github.com/joestump/python-oauth2
+	* requests - https://pypi.python.org/pypi/requests
+
+* Issues:
+	If you get the following exceptions while using requests module run the following command:
+	
+	InsecurePlatformWarning or SNIMissingWarning
+
+	```bash
+
+		pip install pyopenssl ndg-httpsclient pyasn1
+
+	```
--- rocketrip_assignment.py
+++ rocketrip_assignment.py
@@ -1,5 +1,21 @@
+# https://github.com/joestump/python-oauth2
 import oauth2
+# https://pypi.python.org/pypi/requests
 import requests
+# https://docs.python.org/3.5/library/
+import sys
+import random
+
+"""
+
+  Author : Pranav Dhar
+  Date : 03/07/16
+  Description : A Simple python program ( function ) that returns a random tweet based on 'keyword' search. 
+  Usage : python rocketrip_assignment "keyword"
+
+  Documentation for Search API: https://dev.twitter.com/rest/public/search
+
+"""
 
 api_key             = "vW4u1UHOr7T9LloRXbGI9A"
 api_secret          = "Wt1E87vMHENhe54EejfhYNKZwbnscz1iMMVQUdRQ"
@@ -10,7 +26,7 @@ oauth_token    = oauth2.Token(key=access_token_key, secret=access_token_secret)
 oauth_consumer = oauth2.Consumer(key=api_key, secret=api_secret)
 signature_method_hmac_sha1 = oauth2.SignatureMethod_HMAC_SHA1()
 
-def fetch_tweets(url, keyword, http_method, count=100):
+def fetch_tweets(url, keyword, http_method, count=15):
   
   parameters = {"q" : keyword, "count" : count, "include_entities" : "true"}
 
@@ -25,11 +41,31 @@ def fetch_tweets(url, keyword, http_method, count=100):
   return requests.get(req.to_url())
 
 def process_and_return_random_tweet(url, keyword):
-  response = fetch_tweets(endpoint, keyword, "GET")
-  print(response.json())
-	
+  response = fetch_tweets(endpoint, keyword, "GET", 15)
+  
+  # Basic check for valid response status
+  if(response.status_code != 200):
+    print("Please verify credentials.")
+    return
+
+  response_json = response.json()
+
+  tweets = response_json["statuses"]
+  
+  if len(tweets) == 0:
+    print("No tweets matching keyword: %s" % keyword)
+    return
+
+  print random.sample(tweets, 1)
+
 if __name__ == "__main__":
   endpoint = "https://api.twitter.com/1.1/search/tweets.json"
-  keyword  = "football" 
-  process_and_return_random_tweet(endpoint, keyword)
+  keyword  = "football"
 
+  if len(sys.argv) == 2:
+    keyword = sys.argv[1]
+  else:
+    print("Usage : python rocketrip_assignment 'keyword' ")
+    # return 
+
+  process_and_return_random_tweet(endpoint, keyword)
